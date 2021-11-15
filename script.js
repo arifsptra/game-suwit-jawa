3bd46d600d5047812599ec85afa96e2a22571005
arifsptra <111202113480@mhs.dinus.ac.id>
2021-11-15 14:04:48 +0700

    Create code rules algoritms for games

---
+++ script.js
@@ -0,0 +1,10 @@
+// variabel to repeat
+var repeat = true;
+while (repeat) {
+  // capture player choice
+  // capture random computer choice
+  // determine the rules
+  // code to display the result
+  // code to repeat the game
+}
+// message after exiting the game
