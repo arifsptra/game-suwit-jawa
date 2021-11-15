2d9fd655699cfe505be9061a5aabd49a61639af1
arifsptra <111202113480@mhs.dinus.ac.id>
2021-11-15 14:14:15 +0700

    capture random computer choice use Math.random 0 - 1

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ while (repeat) {
   // capture player choice
   var player = prompt("choose one: 'gajah', 'orang', 'semut'");
   // capture random computer choice
+  var computer = Math.random();
+  if (computer < 0.33) {
+    computer == "gajah";
+  } else if (computer < 0.66) {
+    computer == "orang";
+  } else {
+    computer == "semut";
+  }
   // determine the rules
   // code to display the result
   // code to repeat the game
